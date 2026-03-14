4312ac47bf44f29a8653c1c0e17628d96d0e10b0
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-14 14:45:50 +0530

    feat: setup prisma

--- flake.nix
+++ flake.nix
@@ -22,7 +22,19 @@
           prettier
           typescript-language-server
           tailwindcss-language-server
+          openssl
+          prisma-language-server
+          prisma
+          prisma-engines
+
         ];
+        shellHook = ''
+          export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
+          export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+        '';
       };
     };
 }
